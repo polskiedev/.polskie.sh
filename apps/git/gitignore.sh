99927cb4ad09e4b7376fa8f762402126884a630e
John Paul Fernandez <johnpaul.fernandez@newbook.cloud>
2024-07-04 11:55:32 +0800

    DEV-000001: Add copy git from dotfile and command to open autoload

--- apps/git/gitignore.sh
+++ apps/git/gitignore.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+add_gitignore() {
+	local dir_path="$1"
+    if [ -n "$dir_path" ]; then
+		if [[ -d "$dir_path" ]]; then
+        	cp "$PATH_DOTFILES/git/.gitignore" "$dir_path"
+		fi
+	else
+		cp "$PATH_DOTFILES/git/.gitignore" "$PWD"
+    fi
+}
