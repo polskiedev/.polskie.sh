167a7969ffcc227da4f268686936870a6452842c
John Paul Fernandez <johnpaul.fernandez@newbook.cloud>
2024-07-05 10:39:10 +0800

    DEV-000001: Add cd grouping

--- commands/cd/cd.sh
+++ commands/cd/cd.sh
@@ -1,46 +1,76 @@
 #!/bin/bash
 
-get_pathinfo_cd_list() {
+pathinfo_override_command_cd() {
     declare -A pathinfo
 
     pathinfo["destination"]="$ENV_TMP_DIR/$ENV_TMP_LIST"
+    pathinfo["filename"]="cd.txt"
     
     # Return associative array
     echo "$(declare -p pathinfo)"
 }
 
-dump_pathinfo_cd_list() {
-    # Evaluate the associative array returned by get_pathinfo_cd_list
-    eval "$(get_pathinfo_cd_list)"
+cleanup_override_command_cd() {
+    # Unset the array, to prevent leakage
+    unset pathinfo
+}
+
+dump_pathinfo_override_command_cd() {
+    # Evaluate the associative array returned by get_pathinfo_cd
+    eval "$(pathinfo_override_command_cd)"
 
     # Loop through each item in the associative array
     for key in "${!pathinfo[@]}"; do
         echo "$key: ${pathinfo[$key]}"
     done
+
+    cleanup_override_command_cd
 }
 
-add_cd_list() {
-    eval "$(get_pathinfo_cd_list)"
+add_to_list_override_command_cd() {
+    eval "$(pathinfo_override_command_cd)"
 
 	local dest_path="${pathinfo['destination']}"
-	local filename="cd.txt"
-	local filepath="$dest_path/$filename"
+	local filename="${pathinfo['filename']}"
+    local group_filename="$filename"
 	local type="list"
+    local group="$1" 
+
+    if [[ -n "$group" ]]; then
+        group_filename="cd.group.${group}.txt"
+        # if [[ -f "$dest_path/$group_filename" ]]; then
+            filename="$group_filename"
+        # fi
+    fi
 
 	add_to_temp "$type" "$filename" "$PWD"
 
+    cleanup_override_command_cd
+
+	# local filepath="$dest_path/$filename"
 	# echo "================="
 	# echo "History: '$filepath'"
 	# cat $filepath
+
 }
 
-show_cd_list() {
-    # Evaluate the associative array returned by get_pathinfo_cd_list
-    eval "$(get_pathinfo_cd_list)"
+go_to_list_override_command_cd() {
+    # Evaluate the associative array returned by get_pathinfo_cd
+    eval "$(pathinfo_override_command_cd)"
 
 	local dest_path="${pathinfo['destination']}"
-	local filename="cd.txt"
-	local filepath="$dest_path/$filename"
+	local filename="${pathinfo['filename']}"
+    local group_filename="$filename"
+    local group="$1" 
+
+    if [[ -n "$group" ]]; then
+        group_filename="cd.group.${group}.txt"
+        if [[ -f "$dest_path/$group_filename" ]]; then
+            filename="$group_filename"
+        fi
+    fi
+
+    local filepath="$dest_path/$filename"
 
     # Use fzf to select a directory from the list
     selected_dir=$(fzf  --prompt="Go to what directory? " < "$filepath")
@@ -53,31 +83,40 @@ show_cd_list() {
     else
         echo "No directory selected."
     fi
+
+    cleanup_override_command_cd
 }
 
-override_change_directory() {
+override_command_cd() {
     declare -A result
     declare -a remaining_parameters
-    local requested_vars=("psh")
+    local requested_vars=("psh" "showdata")
     local args=("$@")
 
-	IFS=','; joined_string="${args[*]}"; unset IFS
-	echo "Passed Arguments: ($joined_string)"
-
     process_args result remaining_parameters requested_vars[@] "${args[@]}"
+    count=${#remaining_parameters[@]}
+
+    if [[ "${result["showdata"]}" = true ]]; then
+        IFS=','; joined_string="${args[*]}"; unset IFS
+        echo "Passed Arguments: ($joined_string)"
+
+        echo "Request Parameters:"
+        for key in "${!result[@]}"; do
+            echo "$key: ${result[$key]}"
+        done
+
+        echo "Remaining Parameters:"
+        for param in "${remaining_parameters[@]}"; do
+            echo "$param"
+        done
+    fi
 
     if [[ "${result["psh"]}" = true ]]; then
         echo "override"
+    # elif [ $count -eq 0 ]; then
+    #     echo "cd: No arguments passed."
     else
-        echo "process command"
+        # Just pass it all to cd
+        command cd "$@"
     fi
-    # echo "Request Parameters:"
-    # for key in "${!result[@]}"; do
-    #     echo "$key: ${result[$key]}"
-    # done
-
-    # echo "Remaining Parameters:"
-    # for param in "${remaining_parameters[@]}"; do
-    #     echo "$param"
-    # done
 }
